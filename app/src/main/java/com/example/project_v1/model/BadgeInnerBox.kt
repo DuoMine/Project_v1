224d015051175ba1ffad88037375ab1f893f4fbb
DuoMine <90735844+DuoMine@users.noreply.github.com>
2023-12-18 10:10:44 +0900

    Delete app directory

--- app/src/main/java/com/example/project_v1/model/BadgeInnerBox.kt
+++
@@ -1,6 +0,0 @@
-package com.example.project_v1.model
-
-import java.io.Serializable
-
-data class BadgeInnerBox(val badgeName:String ="", val badgeTitle:String="", val badgeDrawableID:Int=0):Serializable {
-}
